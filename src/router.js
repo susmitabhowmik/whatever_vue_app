2315f433a6d13334486559915eeb3d59b5f0a451
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-04-21 19:17:35 -0400

    add localEvents view

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Restaurants from './views/Restaurants.vue';
 import majorEvents from './views/majorEvents.vue';
+import localEvents from './views/localEvents';
 
 Vue.use(Router);
 
@@ -26,12 +27,9 @@ export default new Router({
       component: majorEvents
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/localEvents',
+      name: 'localEvents',
+      component: localEvents
     }
   ]
 });
--- src/views/localEvents.vue
+++ src/views/localEvents.vue
@@ -3,20 +3,17 @@
     <h1>{{ message }}</h1>
     <p>Please enter your address: <input type="text" v-model="address" /></p>
     <p>Please enter a radius: <input type="text" v-model="radius" /></p>
-    <p>Please enter a start date: <input type="dateTime-local" v-model="start_date" /></p>
+     <p>Please enter a start date: <input type="dateTime-local" v-model="start_date" /></p>
     <p>Please enter an end date: <input type="dateTime-local" v-model="end_date" /></p>
 
 <!--     <button v-on:clcick ="toggleParams()">Refine Search</button> 
  -->    <p><button v-on:click="addParams()"> Go! </button></p>
-    <p>Name: {{majorEvent.name}}</p>
-    <p>Date/Time {{majorEvent.dateTime}}</p>
-    <p>Address: {{majorEvent.address}}</p>
-    <p>Venue: {{majorEvent.venue}}</p>
-    <p>Price Range: {{majorEvent.priceRange}}</p>
-    <p>URL: {{majorEvent.url}}</p>
-
-
-
+    <p>Name: {{localEvent.event_name}}</p>
+    <p>Description: {{localEvent.description}}</p>
+    <p>Start Time: {{localEvent.start_time}}</p>
+    <p>End Time: {{localEvent.end_time}}</p>
+    <p>Venue: {{localEvent.venue}}</p>
+    <p>URL: {{localEvent.url}}</p>
   </div>
 </template>
 
@@ -29,8 +26,8 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Find a major event near you!",
-      majorEvent: [],
+      message: "Find a local event near you!",
+      localEvent: [],
     };
   },
   created: function() {
@@ -38,9 +35,9 @@ export default {
   methods: {
     addParams: function() {
       console.log('retrieving data...');
-      axios.get("/api/events?address=" + this.address + "&radius=" + this.radius + "&startDateTime=" + this.start_date + "&endDateTime=" + this.end_date).then(response => {
+      axios.get("/api/events2?address=" + this.address + "&radius=" + this.radius + "&startDateTime=" + this.start_date + "&endDateTime=" + this.end_date).then(response => {
         console.log(response);
-        this.majorEvent = response.data;
+        this.localEvent = response.data;
       });
     }
   }
