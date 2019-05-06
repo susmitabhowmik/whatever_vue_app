e2ec82a4a4101f00848140ad21e8c76b2f186afa
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-05-05 23:26:26 -0400

    updated dynamic map

--- src/views/Maps.vue
+++ src/views/Maps.vue
@@ -1,9 +1,39 @@
 <template>
- <div id='map' style='width: 100%; height: 500px; background-color: black'></div>
+  <div>
+ <div id='map'></div>
+ <div id='instructions'></div>
+</div>
 </template>
 
 <style>
-#map { position:relative; top:0; bottom:0; width:100%; }
+ body {
+   margin: 0;
+   padding: 0;
+ }
+    
+ #map {
+   position: absolute;
+   top: 0; 
+   bottom: 0; 
+   width: 100%;
+ }
+ #instructions {
+  position: relative;
+  margin: 20px;
+  width: 25%;
+  top: 50%;
+  bottom: 20%;
+  padding: 20px;
+  background-color: rgba(255, 255, 255, 0.9);
+  overflow-y: scroll;
+  font-family: sans-serif;
+  font-size: 0.8em;
+  line-height: 2em;
+}
+
+.duration {
+  font-size: 2em;
+}
 </style>
 
 <script>
@@ -12,15 +42,167 @@ export default {
   mounted: function() {
     mapboxgl.accessToken = `${process.env.VUE_APP_MAPBOX_ACCESS_TOKEN}`;
     var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-79.4512, 43.6568],
-      zoom: 13
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v11', //stylesheet location
+      center: [-122.662323,45.523751], // starting position
+      zoom: 12, // starting zoom
     });
-       
-    map.addControl(new MapboxDirections({
-      accessToken: mapboxgl.accessToken
-    }), 'top-left');
-  }
+ 
+    // set the bounds of the map
+    var bounds = [[-123.069003,45.395273],[-122.303707,45.612333]];
+    map.setMaxBounds(bounds);
+ 
+    // initialize the map canvas to interact with later
+    var canvas = map.getCanvasContainer();
+ 
+    // an arbitrary start will always be the same
+    // only the end or destination will change
+    var start = [-122.662323,45.523751];
+ 
+    // create a function to make a directions request
+    function getRoute(end) {
+      // make directions request using cycling profile
+      var url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+
+      // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+      var req = new XMLHttpRequest();
+      req.responseType = 'json';
+      req.open('GET', url, true);
+      req.onload  = function() {
+        var data = req.response.routes[0];
+        var route = data.geometry.coordinates;
+        var geojson = {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "LineString",
+            "coordinates": route
+          }
+        };
+        // if the route already exists on the map, we'll reset it using setData
+        if (map.getSource('route')) {
+          map.getSource('route').setData(geojson);
+        }
+        // otherwise, we'll make a new request
+        else {
+          map.addLayer({
+            "id": "route",
+            "type": "line",
+            "source": {
+              "type": "geojson",
+              "data": {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                  "type": "LineString",
+                  "coordinates": geojson
+                }
+              }
+            },
+            "layout": {
+              "line-join": "round",
+              "line-cap": "round"
+            },
+            "paint": {
+              "line-color": "#3887be",
+              "line-width": 5,
+              "line-opacity": 0.75
+            }
+          });
+        };
+ 
+        // get the sidebar and add the instructions
+        var instructions = document.getElementById('instructions');
+        var steps = data.legs[0].steps;
+ 
+        var tripInstructions = [];
+        for (var i = 0; i < steps.length; i++) {
+          tripInstructions.push('<br><li>' + steps[i].maneuver.instruction) + '  </li>';
+          instructions.innerHTML = '<br><span class="duration">Trip duration: ' + Math.floor(data.duration/60) + ' min 🚴 </span>' + tripInstructions;
+        };
+      };
+      req.send();
+    };
+
+    map.on('load', function() {
+    // make an initial directions request that
+    // starts and ends at the same location
+      getRoute(start);
+  
+      // Add destination to the map
+      map.addLayer({
+        "id": "point",
+        "type": "circle",
+        "source": {
+          "type": "geojson",
+          "data": {
+            "type": "FeatureCollection",
+            "features": [{
+              "type": "Feature",
+              "properties": {},
+              "geometry": {
+                "type": "Point",
+                "coordinates": start
+              }
+            }
+            ]
+          }
+        },
+        "paint": {
+          "circle-radius": 10,
+          "circle-color": "#3887be"
+        }
+      });
+
+      // allow the user to click the map to change the destination
+      map.on('click', function(e) {
+        var coordsObj = e.lngLat;
+        canvas.style.cursor = '';
+        var coords = Object.keys(coordsObj).map(function(key) { 
+          return   coordsObj[key];
+        });
+        var end = {
+          "type": "FeatureCollection",
+          "features": [{
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+              "type": "Point",
+              "coordinates": coords
+            }
+          }
+          ]
+        };
+        if (map.getLayer('end')) {
+          map.getSource('end').setData(end);
+        } else {
+          map.addLayer({
+            "id": "end",
+            "type": "circle",
+            "source": {
+              "type": "geojson",
+              "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                  "type": "Feature",
+                  "properties": {},
+                  "geometry": {
+                    "type": "Point",
+                    "coordinates": coords
+                  }
+                }
+                ]
+              }
+            },
+            "paint": {
+              "circle-radius": 10,
+              "circle-color": "#f30"
+            }
+          });
+        }
+        getRoute(coords);
+      });
+    });
+  },
 };
 </script>
